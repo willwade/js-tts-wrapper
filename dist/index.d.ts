92e747f0df87a98d55f082942b54c493dce74813
will wade <willwade@gmail.com>
2025-04-08 08:26:50 +0100

    remove stuff that shouldnt be in this library

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { AbstractTTSClient } from "./core/abstract-tts";
-export * as SSMLUtils from "./core/ssml-utils";
-export * as VoiceUtils from "./core/voice-utils";
-export { AudioPlayback } from "./core/playback";
-export { SSMLBuilder } from "./ssml/builder";
-export * as SpeechMarkdown from "./markdown/converter";
-export { AzureTTSClient } from "./engines/azure";
-export type { SpeakOptions, UnifiedVoice, TTSCredentials, TTSEventType, WordBoundaryCallback, SimpleCallback, PropertyType, } from "./types";
